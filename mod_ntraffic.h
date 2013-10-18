1fad6bf8964b5906ee07408f5e6a6aafe8080153
Massimo Cetra <mcetra@gmail.com>
2013-10-18 10:02:53 +0200

    Bump version and add credits

--- mod_ntraffic.h
+++ mod_ntraffic.h
@@ -1,5 +1,32 @@
+/*
+ * mod_ntraffic - Traffic statistics collector for Apache
+ *
+ * Copyright (C) 2008-2013, Massimo Cetra <massimo.cetra at gmail.com>
+ *
+ * Version: MPL 2
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mod_ntraffic
+ *
+ * The Initial Developer of the Original Code is
+ * Massimo Cetra <massimo.cetra at gmail.com>
+ *
+ * Portions created by the Initial Developer are Copyright (C)
+ * the Initial Developer. All Rights Reserved.
+ *
+ */
 
 #include <assert.h>
+
 #include "httpd.h"
 #include "http_config.h"
 #include "http_request.h"
@@ -8,17 +35,21 @@
 #include "http_main.h"
 #include "http_log.h"
 #include "unixd.h"
+
 #include "mod_core.h"
+
+#include "ap_release.h"
 #include "ap_mpm.h"
+#include "scoreboard.h"
+
 #include "apr_strings.h"
 #include "apr_atomic.h"
-#include "scoreboard.h"
 
 /* Uncomment to be flooded with error messages (to debug, obviously */
 //#define DEBUG             1
 
 #define MODULE_NAME		"mod_ntraffic"
-#define MODULE_VERSION 		"0.5"
+#define MODULE_VERSION 		"0.6"
 #define MODULE_COMPONENT_STR 	"mod_ntraffic/0.5"
 #define DEFAULT_DATA_DIR 	"/www/var/spool/apache2/mod_traffic"
 #define DEFAULT_UPDATE_INTERVAL 60
--- shm_pool.c
+++ shm_pool.c
@@ -1,3 +1,29 @@
+/*
+ * mod_ntraffic - Traffic statistics collector for Apache
+ *
+ * Copyright (C) 2008-2013, Massimo Cetra <massimo.cetra at gmail.com>
+ *
+ * Version: MPL 2
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mod_ntraffic
+ *
+ * The Initial Developer of the Original Code is
+ * Massimo Cetra <massimo.cetra at gmail.com>
+ *
+ * Portions created by the Initial Developer are Copyright (C)
+ * the Initial Developer. All Rights Reserved.
+ *
+ */
 
 #include "mod_ntraffic.h"
 
